1b025630b9c3f4b4fc3eb851ff89153829a985b5
Ashwin <ashwin.chinnu@ei.study>
2024-03-29 14:27:26 +0530

    added files

--- simpleapp/src/app/app.module.ts
+++ simpleapp/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AddTaskComponent,
+    ButtonComponent,
+    FooterComponent,
+    HeaderComponent,
+    TaskItemComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
