10fc73f9c267b1ec6cf04bddecd7374de46ba0ff
Ashwin <ashwinrockers378@gmail.com>
2024-03-31 00:59:11 +0530

    changes:   - new file:   package-lock.json   - modified:   simpleapp/package-lock.json   - modified:   simpleapp/package.json   - modified:   simpleapp/src/app/app.component.html   - modified:   simpleapp/src/app/components/task-item/task-item.component.html   - modified:   simpleapp/src/app/components/tasks/tasks.component.ts   - deleted:    simpleapp/src/app/services/task.service.spec.ts   - modified:   simpleapp/src/app/services/task.service.ts   - deleted:    simpleapp/src/app/services/ui.service.spec.ts

--- simpleapp/src/app/components/tasks/tasks.component.ts
+++ simpleapp/src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { TASKS } from 'src/mock-tasks';
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service'
 import { Task } from 'src/Task';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
-  constructor() { }
+export class TasksComponent implements OnInit{
+  tasks: Task[] = [];
+  constructor(private taskservice:TaskService) { }
+  ngOnInit(): void {
+     this.taskservice.gettasks().subscribe((tasks) => this.tasks = tasks);
+     
+  }
   // deleteTask(task: Task) {
   //   this.tasks = this.tasks.filter(t => t.id !== task.id);
   // }
--- simpleapp/src/app/services/task.service.ts
+++ simpleapp/src/app/services/task.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { TASKS } from 'src/mock-tasks';
+import { Task } from 'src/Task';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
+  
   constructor() { }
+  gettasks():Observable<Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
